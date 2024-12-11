a522e2ced0fd3120e5bac7e973d3c6eca7243966
Jeremy <jeremy.bluhm@protonmail.com>
2024-12-11 23:33:55 +0100

    added day 03

---
+++ AdventOfCode2024/Day03.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024;
+
+public class Day03(string filePath) : AdvenfOfCodeDay(filePath)
+{
+
+    public override void Part1()
+    {
+        var pattern = @"mul\(\d{1,3},\d{1,3}\)";
+        var matches = Regex.Matches(this.Input, pattern);
+        var total = 0;
+        foreach (Match match in matches)
+        {
+            var values = match.Value.Replace("mul(", "").Replace(")", "").Split(",");
+            total += int.Parse(values[0]) * int.Parse(values[1]);
+        }
+        Console.WriteLine($"Total part 1: {total}");
+    }
+
+    public override void Part2()
+    {
+        var pattern = @"do\(\)|don't\(\)|mul\(\d{1,3},\d{1,3}\)";
+        
+        var matches = Regex.Matches(this.Input, pattern);
+        
+        var total = 0;
+
+        var isDo = true;
+        foreach (Match match in matches)
+        {
+            if (match.Value == "do()")
+            {
+                isDo = true;
+                continue;
+            } else 
+            if (match.Value == "don't()")
+            {
+                isDo = false;
+                continue;
+            }
+
+            if (isDo)
+            {
+                var values = match.Value.Replace("mul(", "").Replace(")", "").Split(",");
+                total += int.Parse(values[0]) * int.Parse(values[1]);   
+            }
+        }
+        
+        Console.WriteLine($"Total part 2: {total}");
+    }
+}
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -1,4 +1,11 @@
 ﻿using AdventOfCode2024;
 
+Console.WriteLine("Advent of Code 2024");
+Console.WriteLine("Day 1:");
 new Day01("inputs/day01.txt").Run();
+Console.WriteLine("====================================");
+Console.WriteLine("Day 2:");
 new Day02("inputs/day02.txt").Run();
+Console.WriteLine("====================================");
+Console.WriteLine("Day 3:");
+new Day03("inputs/day03.txt").Run();
